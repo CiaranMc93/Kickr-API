0f37fc3957173cf5e48cfa19d0668566eb103013
LOYCORP\cmcmanus <cmcmanus@loyalty.com>
2017-07-21 15:55:48 -0400

    Making more changes. Getting to know the language.

---
+++ src/main/java/Spring/User.java
@@ -0,0 +1,38 @@
+package Spring;
+
+public class User 
+{
+    private String password;
+    private String username;
+
+    public User()
+    {
+
+    }
+
+    public User(String username,String password)
+    {
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getUser()
+    {
+        return password;
+    }
+
+    public void setUser(String user)
+    {
+        this.password = user;
+    }
+
+    public String getName()
+    {
+        return username;
+    }
+
+    public void setName(String name)
+    {
+        this.username = name;
+    }
+}
