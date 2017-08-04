ce307c9503ba120276a2c1f67e5b089c8e005876
LOYCORP\cmcmanus <cmcmanus@loyalty.com>
2017-08-04 12:10:37 -0400

    Created a way to scrape the data and parse to JSON.

--- src/main/java/Spring/FixtureList.java
+++ src/main/java/Spring/FixtureList.java
@@ -1,21 +1,17 @@
 package Spring;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,40 +27,79 @@ public class FixtureList {
 		//get the correct county URL
 		if(county.toLowerCase().equals("laois"))
 		{
-			URL = "http://www.laoisgaa.ie/upcomingFixtures/";
-		}
-		 
-		try 
-		{
-			 //jsoup gets the html page as a document
-			 Document doc = Jsoup.connect(URL).get();
+			//define our lists
+			ArrayList<JSONObject> matches = new ArrayList<JSONObject>();
+			ArrayList<JSONObject> competition = new ArrayList<JSONObject>();
+			ArrayList<JSONObject> date = new ArrayList<JSONObject>();
+			ArrayList<JSONObject> matchAll = new ArrayList<JSONObject>();
 			
-			 //item odd is the tr for each match.
-			 Elements trMatch = doc.getElementsByClass("item odd");
-			 Elements trCompetition = doc.getElementsByClass("competition");
-			 Elements trDate = doc.getElementsByClass("date");
-		
-			 for (Element trElem : trMatch) 
-			 {
-			        //System.out.println(trElem.toString());
-			 }
-			 
-			 for (Element trElem : trCompetition) 
-			 {
-				 	
-				 	System.out.println("Size: " + trElem.child(0).html().toString());
-			 }
-			 
-			 for (Element trElem : trDate) 
-			 {
-			        System.out.println(trElem.childNodes().toString());
-			 }
-		
-		//obj = new JSONObject();
-		}
-		catch(Exception e)
-		{
-		    System.out.println("Got here" + e);
-		}
+			JSONObject match = null;
+			JSONObject comp = null;
+			JSONObject dates = null;
+			JSONObject matchInfo = null;
+			JSONObject matchOvr = new JSONObject();
+			
+			
+			URL = "http://www.laoisgaa.ie/upcomingFixtures/";
+			
+			try 
+			{
+				 //jsoup gets the html page as a document
+				 Document doc = Jsoup.connect(URL).get();
+				
+				 //extract the information via the element names
+				 Elements trMatch = doc.getElementsByClass("item odd");
+				 Elements trCompetition = doc.getElementsByClass("competition");
+				 Elements trDate = doc.getElementsByClass("date");
+				 
+				 //for each match in the webpage
+				 for (Element trElem : trMatch) 
+				 {
+					 match = new JSONObject();
+					 
+					 match.put("Time", trElem.child(0).html().toString());
+					 match.put("Home", trElem.child(1).html().toString());
+					 match.put("Away", trElem.child(3).html().toString());
+					 match.put("Venue", trElem.child(4).html().toString());
+
+					 //add to list
+					 matches.add(match);
+				 }
+				 
+				 //for each competition in the webpage
+				 for (Element trElem : trCompetition) 
+				 {
+					 comp = new JSONObject();
+					 
+					 comp.put("Competition",trElem.child(0).html().toString());
+					 competition.add(comp);
+				 }
+				 
+				 //for each date in the webpage
+				 for (Element trElem : trDate) 
+				 {
+					 dates = new JSONObject();
+					 
+					 dates.put("Date",trElem.child(0).html().toString());
+					 date.add(dates);
+				 }
+				 
+				 //loop through the list of strings and create the JSONObject/Array
+				 for(int i=0; i<35; i++)
+				 {
+					 //create the main object
+					 matchAll.add(matches.get(i));
+					 matchAll.add(competition.get(i));
+					 matchAll.add(date.get(i));
+				 }
+
+				 System.out.println("JSON:" + matchAll.toString());
+				 
+			}
+			catch(Exception e)
+			{
+			    System.out.println("Got here" + e);
+			}
+		}	
     }
 }
