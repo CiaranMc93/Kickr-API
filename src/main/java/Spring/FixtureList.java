2bf1ddb72afb2864ccc793560306fab3180b00f0
LOYCORP\cmcmanus <cmcmanus@loyalty.com>
2018-01-24 11:53:04 -0500

    Adding inter-county matches.

--- src/main/java/Spring/FixtureList.java
+++ src/main/java/Spring/FixtureList.java
@@ -1,7 +1,9 @@
 package Spring;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,22 +30,6 @@ public class FixtureList {
 		if(county.toLowerCase().equals("laois"))
 		{
 			URL = "http://www.laoisgaa.ie/upcomingFixtures/";
-			
-			try 
-			{
-				 //jsoup gets the html page as a document
-				 Document doc = Jsoup.connect(URL).get();
-				 
-				 return getMatches(doc,"fixtures",county);
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-		else if(county.toLowerCase().equals("kerry"))
-		{
-			URL = "http://www.kerrygaa.ie/upcomingFixtures";
 			
 			try 
 			{
@@ -105,10 +91,191 @@ public class FixtureList {
 				e.printStackTrace();
 			}
 		}
+		else if(county.toLowerCase().equals("county"))
+		{
+			URL = "https://www.rte.ie/sport/results/gaa/2018/6119/fixtures/";
+			
+			try 
+			{
+				 //jsoup gets the html page as a document
+				 Document doc = Jsoup.connect(URL).get();
+				 
+				 return countyMatches(doc,"fixtures","inter-county");
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
 		
 		return null;	
     }
     
+	private ArrayList<Matches> countyMatches(Document doc, String option, String county) 
+	{
+		ArrayList<JSONObject> matches = new ArrayList<JSONObject>();
+		
+		try 
+		{	  
+			 //get the array of matches
+			 matches = countyMatchJSONObject(doc,option,county);
+			 
+			 if(null == matches)
+			 {
+				 return null;
+			 }
+
+			 //array of matches objects
+			 ArrayList<Matches> matchArr = new ArrayList<Matches>();
+			 Matches matchObj;
+			 
+			 //loop through the list of strings and create the JSONObject/Array
+			 for(int i=0; i< matches.size(); i++)
+			 {
+				 //create an object for each match
+				 matchObj = new Matches(matches.get(i));
+				 //create a list of matches
+				 matchArr.add(matchObj);
+			 }	 
+			 
+			 return matchArr;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	private ArrayList<JSONObject> countyMatchJSONObject(Document doc, String option, String county) 
+	{
+		//define our lists
+		ArrayList<JSONObject> matches = new ArrayList<JSONObject>();
+   	 	JSONObject match = null;
+   	    	
+   	 	 //we need temp variables for date and competition
+		 String competition = "";
+		 String date = "";
+		 String time = "";
+		 String venue = "";
+		 String homeTeam = "";
+		 String awayTeam = "";
+		 String homeScore = "";
+		 String awayScore = "";
+		 
+		 try
+		 {
+			 Elements ovrTable = doc.select("*");
+			 
+			 for(Element rows : ovrTable)
+			 {
+				 if(rows.className().equals("date"))
+				 {
+					 date = rows.getElementsByClass("date").text();
+				 }
+				 else if(rows.className().equals("event notstarted"))
+				 {
+					 time = rows.getElementsByClass("time").text();
+
+    				 //get the team names
+    				 Elements homeTeamElement = rows.getElementsByClass("team home-team");
+    				 Elements awayTeamElement = rows.getElementsByClass("team away-team");
+    				 Elements scoreElement = rows.getElementsByClass("score");
+    				 
+    				//get the score if any
+    				 for(Element score : scoreElement)
+    				 {
+    					 if(score.getElementsByClass("score").text().toLowerCase().contains("v"))
+    					 {
+    						 homeScore = "0-00";
+    						 awayScore = "0-00";
+    					 }
+    				 }
+    				 
+    				 //get the home team
+    				 for(Element home : homeTeamElement)
+    				 {
+    					 homeTeam = home.getElementsByClass("team-name").text();
+    				 }
+    				 
+    				 //get the away team
+    				 for(Element away : awayTeamElement)
+    				 {
+    					 awayTeam = away.getElementsByClass("team-name").text();
+    				 }
+    				 
+    				 //get match info
+    				 Elements infoElement = rows.getElementsByClass("additional-meta");
+    				 
+    				 //get the match info
+    				 for(Element info : infoElement)
+    				 {
+    					 competition = info.getElementsByClass("stage").text();
+    					 venue = info.getElementsByClass("venue").text();
+    				 }
+    				 
+    				 if(option.equals("fixtures"))
+    				 {
+    					 match = new JSONObject();
+    					 
+    					 match.put("Time", time);
+    					 match.put("Home", homeTeam);
+    					 match.put("HomeScore", homeScore);
+    					 match.put("Away", awayTeam);
+    					 match.put("AwayScore", awayScore);
+    					 match.put("Venue", venue);
+    					 match.put("Date", date);
+    					 match.put("Competition", competition);
+    					 match.put("Winner", "N/A");
+    					 match.put("County", county);
+    					 
+    					 //add to list
+    					 matches.add(match);
+    				 }
+    				 else if(option.equals("results"))
+    				 {
+    					 match = new JSONObject();
+    					 
+    					 match.put("Time", time);
+    					 match.put("Home", homeTeam);
+    					 match.put("HomeScore", homeScore);
+    					 match.put("Away", awayTeam);
+    					 match.put("AwayScore", awayScore);
+    					 match.put("Venue", venue);
+    					 match.put("Date", date);
+    					 match.put("Competition", competition);
+    					 match.put("County", county);
+    					 
+    					 //select the winning team
+    					 if(winningTeam(homeScore,awayScore, " : ").equals("Home"))
+    					 {
+    						 match.put("Winner", homeTeam);
+    					 }
+    					 else if(winningTeam(homeScore,awayScore, " : ").equals("Away"))
+    					 {
+    						 match.put("Winner", awayTeam);
+    					 }
+    					 else if(winningTeam(homeScore,awayScore, " : ").equals("Draw"))
+    					 {
+    						 match.put("Winner", "Draw");
+    					 }
+    					 else
+    					 {
+    						 match.put("Winner", "N/A");
+    					 }
+    				 }
+				 }
+			 }
+		 }
+		 catch(Exception e)
+		 {
+			 System.out.println("Got here carlow obj" + e);
+		 }
+		 
+		return matches;
+	}
+
 	@CrossOrigin
     //get the results for the county specified
     @RequestMapping(value = "/results/{county}")
@@ -120,22 +287,6 @@ public class FixtureList {
 		if(county.toLowerCase().equals("laois"))
 		{
 			URL = "http://www.laoisgaa.ie/latestResults/";
-			
-			try 
-			{
-				 //jsoup gets the html page as a document
-				 Document doc = Jsoup.connect(URL).get();
-				 
-				 return getMatches(doc,"results",county);
-			}
-			catch(Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-		else if(county.toLowerCase().equals("kerry"))
-		{
-			URL = "http://www.kerrygaa.ie/latestResults";
 			
 			try 
 			{
@@ -379,7 +530,7 @@ public class FixtureList {
 				 {
 					 match = new JSONObject();
 					 
-					 match.put("Time", time);
+					 match.put("Time", parseTime(time));
 					 match.put("Home", homeTeam);
 					 match.put("Away", awayTeam);
 					 match.put("Venue", venue);
@@ -394,7 +545,7 @@ public class FixtureList {
 				 {
 					 match = new JSONObject();
 					 
-					 match.put("Time", time);
+					 match.put("Time", parseTime(time));
 					 match.put("Home", homeTeam);
 					 match.put("HomeScore", homeScore);
 					 match.put("Away", awayTeam);
@@ -467,7 +618,7 @@ public class FixtureList {
 					 {
 						 match = new JSONObject();
 						 
-						 match.put("Time", fixtures.child(0).html().toString());
+						 match.put("Time", parseTime(fixtures.child(0).html().toString()));
 						 match.put("Home", fixtures.child(1).html().toString());
 						 match.put("Away", fixtures.child(3).html().toString());
 						 match.put("Venue", fixtures.child(4).html().toString());
@@ -564,5 +715,29 @@ public class FixtureList {
 		}
 		
 		return "";
+	}
+	
+	private String parseTime(String time)
+	{
+		if(time.contains(":"))
+		{
+			return time;
+		}
+		
+		String parsedTime = "";
+		
+		try
+		{
+			SimpleDateFormat displayFormat = new SimpleDateFormat("HH:mm");
+	        SimpleDateFormat parseFormat = new SimpleDateFormat("hh mm a");
+	        Date date = parseFormat.parse(time);
+	        parsedTime = displayFormat.format(date);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception: " + e);
+		}
+		
+		return parsedTime;
 	}
 }
